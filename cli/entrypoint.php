b78c2c5d2daf6992a3502563f2328c50b2bae566
YuanHeng23 <110651260+YuanHeng23@users.noreply.github.com>
2022-08-19 16:08:53 +1000

    Add files via upload

--- cli/entrypoint.php
+++ cli/entrypoint.php
@@ -44,11 +44,26 @@ $CFG->tool_forcedcache_config_array = $caches;
 // Instantiate new test generator
 
 // Generate 3 courses
+$course1 = $this->getDataGenerator()->create_course();
+ 
+$category = $this->getDataGenerator()->create_category();
+$course2 = $this->getDataGenerator()->create_course(array('name'=>'Some course', 'category'=>$category->id));
 
 // generate 5 activities in each one
+$course = $this->getDataGenerator()->create_course();
+$generator = $this->getDataGenerator()->get_plugin_generator('mod_page');
+$generator->create_instance(array('course'=>$course->id));
+
+$course = $this->getDataGenerator()->create_course();
+$page = $this->getDataGenerator()->create_module('page', array('course' => $course->id));
 
 // Generate users.
+$user = $this->getDataGenerator()->create_user();
+$user1 = $this->getDataGenerator()->create_user(array('email'=>'user1@example.com', 'username'=>'user1'));
+$this->setUser($user1);
+$this->setGuestUser();
+$this->setAdminUser();
+$this->setUser(null);
 
 
-
-var_dump(cache_helper::get_stats());
+var_dump(cache_helper::get_stats());`
